5d253a4008ad6265d699ddd880aef379bbb197c3
Sandros94 <sandro.circi@digitoolmedia.com>
2024-04-14 13:32:59 +0200

    test: move to nuxt page

--- test/fixtures/basic/app.vue
+++ test/fixtures/basic/app.vue
@@ -1,14 +1,5 @@
 <template>
   <div>
-    <NuxtMarkdown :source="md" :components="{ TestComponent }" />
+    <NuxtPage />
   </div>
 </template>
-
-<script setup>
-import { TestComponent } from '#components'
-const md = `# Nuxt module playground!
-
-Welcome to the example of <a href="https://github.com/sandros94/nuxt-markdown-render" target="_blank">nuxt-markdown-render</a>.
-
-<TestComponent/>`
-</script>
---
+++ test/fixtures/basic/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NuxtMarkdown :source="md" :components="{ TestComponent }" />
+  </div>
+</template>
+
+<script setup>
+import { TestComponent } from '#components'
+const md = `# Nuxt module playground!
+
+Welcome to the example of <a href="https://github.com/sandros94/nuxt-markdown-render" target="_blank">nuxt-markdown-render</a>.
+
+<TestComponent/>`
+</script>
